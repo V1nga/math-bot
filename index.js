d3ac5548664b035a299c6c9c00c7a054814c162c
V1nga <43814565+V1nga@users.noreply.github.com>
2025-06-23 18:55:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const App = require("./src/app");
 
-new App("6811063530:AAEZNhCPYyW_ohwJvj3uWUWbHhSJIGYRjN0").start();
+const key = "";
+
+new App(key).start();
 
 // const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 
